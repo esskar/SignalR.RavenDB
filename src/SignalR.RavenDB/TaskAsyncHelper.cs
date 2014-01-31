3e99833d58fbe566c5f0b472086b5c69bfd367a5
esskar <sk@intertivity.com>
2014-01-31 02:05:23 +0100

    add a mini version of TaskAsyncHelper

---
+++ src/SignalR.RavenDB/TaskAsyncHelper.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See https://github.com/SignalR/SignalR/blob/master/LICENSE.md for license information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace SignalR.RavenDB
+{
+    internal static class TaskAsyncHelper
+    {
+        public static Task Empty = FromResult<object>(null);
+
+        public static Task<T> FromResult<T>(T value)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            tcs.SetResult(value);
+            return tcs.Task;
+        }
+
+        internal static Task FromError(Exception e)
+        {
+            return FromError<object>(e);
+        }
+
+        internal static Task<T> FromError<T>(Exception e)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            tcs.SetUnwrappedException<T>(e);
+            return tcs.Task;
+        }
+
+        internal static void SetUnwrappedException<T>(this TaskCompletionSource<T> tcs, Exception e)
+        {
+            var aggregateException = e as AggregateException;
+            if (aggregateException != null)
+            {
+                tcs.SetException(aggregateException.InnerExceptions);
+            }
+            else
+            {
+                tcs.SetException(e);
+            }
+        }
+
+        internal static bool TrySetUnwrappedException<T>(this TaskCompletionSource<T> tcs, Exception e)
+        {
+            var aggregateException = e as AggregateException;
+            return aggregateException != null 
+                ? tcs.TrySetException(aggregateException.InnerExceptions) 
+                : tcs.TrySetException(e);
+        }
+    }
+}
