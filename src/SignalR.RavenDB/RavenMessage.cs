55ad1c61149678f8c0317f4bfbeb3f6e9a5baadc
esskar <sk@intertivity.com>
2014-01-31 02:07:05 +0100

    define the raven bus message

---
+++ src/SignalR.RavenDB/RavenMessage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR.Messaging;
+
+namespace SignalR.RavenDB
+{
+    public class RavenMessage
+    {
+        public string Id { get; set; }
+
+        public byte[] Data { get; set; }
+
+        internal RavenMessage FromMessages(IList<Message> messages)
+        {
+            var scaleoutMessage = new ScaleoutMessage(messages);
+            return new RavenMessage
+            {
+                Data = scaleoutMessage.ToBytes()
+            };
+        }
+
+        internal ScaleoutMessage ToScaleoutMessage()
+        {
+            return ScaleoutMessage.FromBytes(this.Data);
+        }
+    }
+}
