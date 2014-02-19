96ff0f66a2a6a62f4cbea94a449864015dc3699f
esskar <sk@intertivity.com>
2014-02-19 01:09:26 +0100

    update version information

--- src/SignalR.RavenDB/AssemblyInfo.cs
+++ src/SignalR.RavenDB/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyDescription("RavenDB messaging backplane for scaling out of ASP.NET SignalR applications in a web-farm.")]
 [assembly: AssemblyProduct("SignalR.RavenDB")]
 [assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
