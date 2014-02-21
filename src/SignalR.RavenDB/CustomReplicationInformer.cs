91eb34e8526235cbec7776724a927a5299c88c44
unknown <sk@intertivity.com>
2014-02-21 15:33:20 +0100

    add custom replication informer for custom failover handling

---
+++ src/SignalR.RavenDB/CustomReplicationInformer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Raven.Client.Connection;
+using Raven.Client.Document;
+
+namespace SignalR.RavenDB
+{
+    class CustomReplicationInformer : ReplicationInformer
+    {
+        private readonly IList<string> _failOverUrls;
+
+        public CustomReplicationInformer(DocumentConvention conventions, IEnumerable<string> failOverUrls) 
+            : base(conventions)
+        {
+            if (failOverUrls == null)
+                throw new ArgumentNullException("failOverUrls");
+            _failOverUrls = failOverUrls.ToList();
+        }        
+    }
+}
--- src/SignalR.RavenDB/RavenScaleoutConfiguration.cs
+++ src/SignalR.RavenDB/RavenScaleoutConfiguration.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using Microsoft.AspNet.SignalR.Messaging;
 using Raven.Client;
 using Raven.Client.Document;
@@ -9,8 +12,25 @@ namespace SignalR.RavenDB
     {
         private readonly Func<IDocumentStore> _documentStoreFactory;
 
-        public RavenScaleoutConfiguration(string connectionStringNameOrUrl)
-            : this(CreateConnectionFactory(connectionStringNameOrUrl)) { }
+        public static RavenScaleoutConfiguration CreateWithFailover(string connectionStringName)
+        {
+            if (string.IsNullOrWhiteSpace(connectionStringName))
+                throw new ArgumentNullException("connectionStringName");
+
+            var defaultConnectionString = GetConnectionStrings(connectionStringName, false).FirstOrDefault();
+            if (string.IsNullOrWhiteSpace(defaultConnectionString))
+                throw new ArgumentException("No connection string found under given name.", "connectionStringName");
+
+            var uris = new List<string> { defaultConnectionString };
+            uris.AddRange(GetConnectionStrings(connectionStringName + "_failover", true));
+            return new RavenScaleoutConfiguration(uris);
+        }
+        
+        public RavenScaleoutConfiguration(string connectionStringName)
+            : this(CreateConnectionFactoryFromConnectionStringNameOrUrl(connectionStringName)) { }
+
+        public RavenScaleoutConfiguration(IEnumerable<string> connectionStringUrls)
+            : this(CreateConnectionFactoryFromUrls(connectionStringUrls)) { }
 
         public RavenScaleoutConfiguration(Func<IDocumentStore> documentStoreFactory)
         {
@@ -32,7 +52,20 @@ namespace SignalR.RavenDB
             get { return _documentStoreFactory; }
         }
 
-        private static Func<IDocumentStore> CreateConnectionFactory(string connectionStringNameOrUrl)
+        private static Func<IDocumentStore> CreateConnectionFactoryFromUrls(IEnumerable<string> connectionStringUrls)
+        {
+            if (connectionStringUrls == null)
+                throw new ArgumentNullException("connectionStringUrls");
+
+            return () =>
+            {
+                var documentStore = new DocumentStore();
+                documentStore.Conventions.ReplicationInformerFactory = s => new CustomReplicationInformer(documentStore.Conventions, connectionStringUrls);
+                return documentStore;
+            };
+        }
+
+        private static Func<IDocumentStore> CreateConnectionFactoryFromConnectionStringNameOrUrl(string connectionStringNameOrUrl)
         {
             if (string.IsNullOrWhiteSpace(connectionStringNameOrUrl))
                 throw new ArgumentNullException("connectionStringNameOrUrl");
@@ -43,17 +76,38 @@ namespace SignalR.RavenDB
                     return CreateConnectionFactoryFromConnectionStringName(connectionStringNameOrUrl);
 
                 var uri = new Uri(connectionStringNameOrUrl, UriKind.Absolute);
-                return () => new DocumentStore {Url = uri.AbsoluteUri};
+                return () => new DocumentStore { Url = uri.AbsoluteUri };
             }
             catch (UriFormatException)
             {
                 return CreateConnectionFactoryFromConnectionStringName(connectionStringNameOrUrl);
-            }                       
+            }
         }
 
         private static Func<IDocumentStore> CreateConnectionFactoryFromConnectionStringName(string connectionStringName)
         {
             return () => new DocumentStore { ConnectionStringName = connectionStringName };
+        }
+
+        private static IEnumerable<string> GetConnectionStrings(string name, bool isPrefix)
+        {
+            for (var i = 0; i < ConfigurationManager.ConnectionStrings.Count; i++)
+            {
+                var settings = ConfigurationManager.ConnectionStrings[i];
+                if (!isPrefix)
+                {
+                    if (settings.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                    {
+                        yield return settings.ConnectionString;
+                        yield break;
+                    }
+                }
+                else
+                {
+                    if (settings.Name.StartsWith(name, StringComparison.OrdinalIgnoreCase))
+                        yield return settings.ConnectionString;
+                }
+            }
         }
     }
 }
